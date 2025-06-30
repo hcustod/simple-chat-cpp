510493fe4d864e7a7808cb7066e34722c9bc550f
Henrique Custodio <h.custodio.dev@gmail.com>
2025-06-30 14:00:39 -0400

    Add: Server and Client files basic functionality complete

---
+++ src/client.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <thread>
+#include <string>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+const int PORT = 5000;
+const char* SERVER_IP = "127.0.0.1";
+
+void recieve_loop(int sock) {
+    char buffer[1024];
+    while (true) {
+        ssize_t bytes = recv(sock, buffer, sizeof(buffer) - 1, 0);
+        if (bytes <= 0) break;
+        buffer[bytes] = '\0';
+        std::cout << "\n" << buffer << "\n>";
+        std::cout.flush();
+    }
+
+}
+
+int main() {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in server_addr{};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
+
+    if (connect(sock, (sockaddr*)&server_addr, sizeof(server_addr)) == -1) { 
+        std::cerr << "Failed to connect to server.\n";
+        return 1;
+    }
+
+    std::thread(recieve_loop, sock).detach();
+
+    std::string input;
+    std::cout << "> ";
+    while (getline(std::cin, input)) {
+        send(sock, input.c_str(), input.length(), 0);
+        std::cout << "> ";
+    }
+
+    close(sock);
+    return 0;
+}
--- src/server.cpp
+++ src/server.cpp
@@ -1,23 +1,37 @@
-#include "iostream"
-#include <arpa/inet.h>
-#include <netinet/in.h>
+#include <iostream>
 #include <thread>
+#include <vector>
 #include <string>
 #include <mutex>
-#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 const int PORT = 8080;
 std::vector<int> clients;
 std::mutex m;
 
 void handle_client(int client_fd) {
+    char buffer[1028];
+    while (true) {
+        ssize_t bytes = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+        if (bytes <= 0) break;
+        buffer[bytes] = '\0';
+        broadcast(buffer, client_fd);
+    }
 
-
+    close(client_fd);
+    std::lock_guard<std::mutex> lock(m);
+    clients.erase(std::remove(clients.begin(), clients.end(), client_fd), clients.end());
 }
 
-void broadcast(const std::string& message, int client_fd) {
-
-
+void broadcast(const std::string& message, int sender_fd) {
+    std::lock_guard<std::mutex> lock(m);
+    for (int client_fd : clients) {
+        if (client_fd != sender_fd) {
+            send(client_fd, message.c_str(), message.length(), 0);
+        }
+    }
 }
 
 int main() {
@@ -27,14 +41,14 @@ int main() {
     // Set server addr
     sockaddr_in addr{};
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
+    addr.sin_port = htons(PORT);
     addr.sin_addr.s_addr = INADDR_ANY;
 
     // Attach server to IP:Port and wait for incoming conns
     bind(server_conn, (struct sockaddr*)&addr, sizeof(addr));
     listen(server_conn, 10);
 
-    std::cout << "Server listening on port " << port << std::endl;
+    std::cout << "Server listening on port " << PORT << std::endl;
 
     //
     while (true) {
